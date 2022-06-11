50f33a00dd46e698a2df2e0d53ae28e1575f4efa
Wesley Wei <wesleytianqiwei@gmail.com>
2022-06-11 11:01:48 -0400

    ingredients

--- ice-cream.js
+++ ice-cream.js
@@ -1,2 +1,6 @@
 const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+iceCreamFlavours.push("root bear")
 console.log(iceCreamFlavours);
+console.log(CreamFlavours[0]);
+console.log(iceCreamFlavours[-1]);
+console.log(iceCreamFlavours.length)
---
+++ ingredients.js
@@ -0,0 +1,15 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+let i=0;
+while (i<ingredients.length) {
+  
+  console.log(ingredients[i])
+  i++
+}
+
+for (let i=0; i<ingredients.length;i++) {
+  console.log(ingredients[i])
+}
+
+for (j=ingredients.length-1;j>=0;j--) {
+  console.log(ingredients[j])
+}
